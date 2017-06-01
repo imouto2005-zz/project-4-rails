4d5ea348334c47148f96163b5cab7c150c1b93e4
Louisa Lee <louisa.leeyh@gmail.com>
2017-06-01 18:11:01 +0800

    Database

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-
+  root :to => "activities#index"
 
   get '/foods/search', to: 'foods#location_search'
   get '/activities/meetups', to: 'activities#get_meetups'

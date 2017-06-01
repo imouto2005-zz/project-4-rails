79615176b80e16c3b940985ba0cc6510deb35a30
Louisa Lee <louisa.leeyh@gmail.com>
2017-06-01 17:37:28 +0800

    idk

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Project4Rails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
     config.middleware.use Rack::Cors do
       allow do
         origins '*'

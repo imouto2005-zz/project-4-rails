3d8e60112bc0be695cafc2115eeb9f7ddc179f5d
Louisa Lee <louisa.leeyh@gmail.com>
2017-06-01 18:36:40 +0800

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   # protect_from_forgery with: :exception
+  puts 'hi hi'
   protect_from_forgery with: :null_session
   respond_to :json
 end
